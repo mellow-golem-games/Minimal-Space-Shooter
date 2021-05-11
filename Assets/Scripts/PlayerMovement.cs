fcc4f4a22b9c207d876802fd79471def38d69688
peterjewicz <peterjewicz@totalwebconnections.com>
2021-05-11 09:45:26 -0500

    add player and movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+  public int speed;
+
+  void Update()  {
+    if (Input.GetMouseButton(0)) {
+      Vector2 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+      Vector2 currentPosition = gameObject.transform.position;
+
+      // manually override non moveable direction
+      target.x = currentPosition.x;
+
+      transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
+  }
+}
