9b32dc047863dc9da7cabdecac5693ea0bb7e07c
peterjewicz <peterjewicz@totalwebconnections.com>
2021-05-12 10:19:16 -0500

    setup base button

---
+++ Assets/Scripts/Ui/FireButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FireButton : MonoBehaviour
+{
+    public void Fire() {
+      Debug.Log("Fire!");
+    }
+}
