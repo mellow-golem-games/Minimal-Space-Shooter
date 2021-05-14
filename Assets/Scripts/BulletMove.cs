886d4e4dadb901dca29807e6e16cd39e59fd1e81
peterjewicz <peterjewicz@totalwebconnections.com>
2021-05-14 09:06:34 -0500

    Bullet creation and movement

---
+++ Assets/Scripts/BulletMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMove : MonoBehaviour
+{
+
+    [SerializeField]
+    private int speed = 6;
+    private Renderer m_Renderer;
+
+    void Start() {
+      m_Renderer = GetComponent<Renderer>();
+    }
+
+    void LateUpdate() {
+      transform.Translate (Vector2.right * speed * Time.deltaTime);
+    }
+
+    void Update() {
+      if (!m_Renderer.isVisible) {
+        Destroy(gameObject);
+      }
+    }
+}
--- Assets/Scripts/Ui/FireButton.cs
+++ Assets/Scripts/Ui/FireButton.cs
@@ -5,7 +5,13 @@ using UnityEngine.UI;
 
 public class FireButton : MonoBehaviour
 {
+    public GameObject playerRef;
+    public GameObject bulletPrefab;
+
     public void Fire() {
-      Debug.Log("Fire!");
+      float playerX = playerRef.transform.position.x;
+      float playerY = playerRef.transform.position.y;
+
+      Instantiate(bulletPrefab, new Vector3(playerX, playerY, 0), Quaternion.identity);
     }
 }
